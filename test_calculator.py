1322a043264f98b1690d7e844a741c6cac7d2ca0
PhathuAuti <53606947+PhathuAuti@users.noreply.github.com>
2019-11-04 13:54:19 +0200

    Changed Variable Names

--- test_calculator.py
+++ test_calculator.py
@@ -1,19 +1,19 @@
 import calculator
 
-def test_add():
+def test_add_zeroes():
     assert calculator.add(0, 0) == 0
 
-def test_add_negative_numbers():
+def test_add_two_negative_numbers():
     assert calculator.add(-1, -1) == -2
 
-def test_add_digits():
+def test_add_two_positive_numbers():
     assert calculator.add(4, 5) == 9
 
-def test_add_more():
+def test_add_many_integers():
     assert calculator.add(1,2,3,4) == 10
 
-def test_multiply_digits():
+def test_multiply_two_integers():
     assert calculator.multiply(1, 2) == 2
 
-def test_multiply_more():
+def test_multiply_many_integers():
     assert calculator.multiply(1,2,3,4) == 24
