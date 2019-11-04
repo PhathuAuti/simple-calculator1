7ed00e7a7c8a22109a8bf15f39b076403c20f7d4
PhathuAuti <53606947+PhathuAuti@users.noreply.github.com>
2019-11-04 13:55:38 +0200

    Removed Unnecessary Variable

--- calculator.py
+++ calculator.py
@@ -1,13 +1,11 @@
 def add(*nums):
-    list_of_numbers = list(nums)
     counter = 0
-    for value in list_of_numbers :
+    for value in nums :
         counter += value
     return counter
 
 def multiply(*nums):
-    list_of_numbers = list(nums)
     count = 1
-    for value in list_of_numbers :
+    for value in nums :
         count *= value
     return count
